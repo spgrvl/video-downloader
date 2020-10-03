b311d704f87944055ca5b53fc32b0f99c89a785f
Spyros <spgrvl@gmail.com>
2020-10-03 09:54:59 +0300

    Added Instagram video downloading functionality.

--- video_downloader.py
+++ video_downloader.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup as soup
+from datetime import datetime
+import requests
 import re
 
 def platform(url):
@@ -8,6 +11,29 @@ def platform(url):
     elif ig_match != None:
         return "ig", ig_match[1]
 
+def instagram(shortcode):
+    page_html = requests.get("https://www.instagram.com/p/" + shortcode).content
+    page_soup = soup(page_html, "html.parser")
+    post_type = page_soup.findAll("meta", {"name" : "medium"})[0]['content']
+    if post_type == "video":
+        video_url = page_soup.findAll("meta", {"property" : "og:video"})[0]['content']
+        response = requests.get(video_url)
+        filename = datetime.strftime(datetime.now(), '%Y%m%d%H%M') + "_ig_" + shortcode
+        with open(filename + ".mp4", "wb") as f:
+            f.write(response.content)
+        print("Download of Instagram Video {} completed.".format(shortcode))
+    else:
+        print("Provided Instagram URL is not a video!")
+
 url = input("\nEnter a Video URL: ")
 platform = platform(url)
-print(platform[0], platform[1])
+
+try:
+    if platform[0] == "ig":
+        instagram(platform[1])
+    elif platform[0] == "fb":
+        facebook(platform[1])
+except TypeError:
+    print("Failed to detect platform of given URL!")
+except Exception as e:
+    print("Video download failed!\n" + str(e))
