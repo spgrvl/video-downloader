076b31eba7c681b502b3f58ec913122baad1a3a2
Spyros <spgrvl@gmail.com>
2020-10-19 23:41:57 +0300

    Added a GUI to the program.

--- video_downloader.py
+++ video_downloader.py
@@ -1,8 +1,39 @@
 from bs4 import BeautifulSoup as soup
+from tkinter import Tk, Text, Label, Entry, Button, StringVar, END
+import argparse
 import requests
 import re
 
-def platform(url):
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--cli", help="Launch the Command Line Interface version of the program", action="store_true")
+args = parser.parse_args()
+
+# Show output in window when in GUI mode or print it when in CLI mode
+_print = print
+def print(msg):
+    if args.cli:
+        _print(msg)
+    else:
+        output.configure(state="normal")
+        output.delete(1.0, END)
+        output.insert(END, msg)
+        output.configure(state="disabled")
+
+def download(url):
+    platform = get_platform(url)
+    try:
+        if platform[0] == "ig":
+            instagram(platform[1])
+        elif platform[0] == "fb":
+            facebook(platform[1])
+        elif platform[0] == "tt":
+            tiktok(platform[1])
+    except TypeError:
+        print("Failed to detect platform of given URL!")
+    except Exception as e:
+        print("Video download failed!\n" + str(e))
+
+def get_platform(url):
     fb_match = re.search(r'^(?:https?:\/\/)?(?:www\.|web\.|m\.)?facebook\..*(?:\/videos.*\/|v=)(\d+).*$', url)
     ig_match = re.search(r'^(?:https?:\/\/)?(?:www\.)?(?:instagram\.com.*\/(?:p|tv|reel)\/)([\d\w\-_]+)(?:\/)?(\?.*)?$', url)
     tt_match = re.search(r'^(?:https?:\/\/)?(?:www\.|m\.)?(?:tiktok\.com.*\/(?:@(?:[\w]+))?(?:v|video|embed|trending)?(?:\/)?(?:video)?(?:\/)?(?:\?shareId=)?)([\d]+)', url)
@@ -60,17 +91,31 @@ def tiktok(video_id):
     else:
         print("Failed to download this TikTok video!")
 
-url = input("\nEnter a Video URL: ")
-platform = platform(url)
+def cli():
+    url = input("\nEnter a Video URL: ")
+    download(url)
 
-try:
-    if platform[0] == "ig":
-        instagram(platform[1])
-    elif platform[0] == "fb":
-        facebook(platform[1])
-    elif platform[0] == "tt":
-        tiktok(platform[1])
-except TypeError:
-    print("Failed to detect platform of given URL!")
-except Exception as e:
-    print("Video download failed!\n" + str(e))
+def gui():
+    global output
+    window = Tk()
+    window.title("Video Downloader")
+
+    url_label = Label(window, text="Video URL:")
+    url_label.grid(row=0, column=0)
+
+    url_value = StringVar()
+    url_input = Entry(window, width=55, textvariable=url_value)
+    url_input.grid(row=0, column=1)
+
+    submit = Button(window, text="Download", height=4, command=lambda: download(url_value.get()))
+    submit.grid(row=0, column=2, rowspan=2)
+
+    output = Text(window, state="disabled", height=3, width=50)
+    output.grid(row=1, column=0, columnspan=2)
+
+    window.mainloop()
+
+if args.cli:
+    cli()
+else:
+    gui()
